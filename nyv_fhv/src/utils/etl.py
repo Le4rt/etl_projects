89d25d1b187a114a6a5076bc0b65b6ac9cfee3bf
Le4rt <Learthyseni100@gmail.com>
2025-11-14 16:30:31 +0100

    Rregullim

--- nyv_fhv/src/utils/etl.py
+++ nyv_fhv/src/utils/etl.py
@@ -8,20 +8,22 @@ import duckdb
 def extract(file_name: str) -> pd.DataFrame: # simple extract function
     return pd.read_csv(f"data/{file_name}", low_memory=False)
 
-def main():
-    engine = get_connection()  #Konektimi ne PostfreSQL
-    df = extract(configuration.FILE_NAME)  #extract csv
-    df = transform(df)
-    load(df, engine)
-    print("Data loaded successfully")
 
 def transform(df):
     #Normalizimi i emrave te kolonave
     df = normalize_column_names(df)
+
+    #Konvertimi i dates
     df["expiration_date"] = pd.to_datetime(df["expiration_date"], format="%m/%d/%Y")
+  
+    #Heq hapesirat e paneojshme ne kolona
     for col in df.select_dtypes(include='object').columns:
         df[col] = df[col].str.strip()
+    
+    #Heq rreshat qe gjinden nga dy her
     df = df.drop_duplicates(subset=['vehicle_license_number', 'dmv_license_plate_number'])
+    
+    #Mban vetem kto kolona
     keep_cols = [
     'vehicle_license_number',
     'license_type',
@@ -33,20 +35,24 @@ def transform(df):
     ]
     df = df[keep_cols]
 
-    con = duckdb.connect()
-    con.register("vhc", df)
+    #Transformimi me Panadaas
+    df = df.sort_values(by="expiration_date", ascending=True)
+    
+    #Transformimi me duckdb
+    # con = duckdb.connect()
+    # con.register("vhc", df)
 
     
-    df = con.execute("""
-    SELECT 
-    --expiration_date
-    --expiration_date, license_type
-    *
-    --*, (expiration_date::DATE - CURRENT_DATE)::INT AS days_until_expiration
-    FROM vhc
-    ORDER BY expiration_date
+    # df = con.execute("""
+    # SELECT 
+    # --expiration_date
+    # --expiration_date, license_type
+    # *
+    # --*, (expiration_date::DATE - CURRENT_DATE)::INT AS days_until_expiration
+    # FROM vhc
+    # ORDER BY expiration_date
     
-    """).fetchdf()
+    # """).fetchdf()
 
     print(df)
 
